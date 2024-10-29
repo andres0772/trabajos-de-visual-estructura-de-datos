f1522bf0a325d6163a4f3800979cd97baedfa864
andres <andresesteb0772@gmail.com>
2024-10-29 13:39:52 -0500

    initial commit

---
+++ scrip.js
@@ -0,0 +1,9 @@
+var num2 = 55
+var num1 = 1
+suma = num2 + num1
+resultado = suma
+console.log("el resultado de la suma fue " + resultado)//resultado de la suma normal
+if(resultado >= 15)//resultado de la suma en booleano
+     {console.log( resultado + 1 + " <= el valor fue mayor a 15 se le sumo 1") }
+else if (resultado <= 15)
+     {console.log(resultado - 4 +  " <= el valor es menor a 15 se le resta 4" )} 
