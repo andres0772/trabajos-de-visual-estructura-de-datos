52827d4185b3a3a2be486924faa438ea4d05d089
andres <andresesteb0772@gmail.com>
2024-10-30 10:55:02 -0500

    primera prueba

--- scrip.js
+++ scrip.js
@@ -7,3 +7,10 @@ if(resultado >= 15)//resultado de la suma en booleano
      {console.log( resultado + 1 + " <= el valor fue mayor a 15 se le sumo 1") }
 else if (resultado <= 15)
      {console.log(resultado - 4 +  " <= el valor es menor a 15 se le resta 4" )} 
+
+
+//ejemplo con decimales
+
+var minutos = 45
+horas = minutos / 60
+console.log("son " + horas +" horas" )
